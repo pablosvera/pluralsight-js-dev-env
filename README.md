f81659012deed4757a523ce56f88f3fdc97dc088
Pablo Vera <pvera@abcconsulting.com.ar>
2017-05-01 22:41:55 -0300

    module 9 work in progress

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pluralsight-js-dev-env
-JavaScript Development Environment from Pluralsight Course
+# js-dev-env-demo
+Course demo
--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -13,15 +15,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
-  if(err) {
+  if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     open('http://localhost:' + port);
   }
 });
